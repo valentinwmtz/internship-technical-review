9c409f7b52d9394033031e882e262ad78d8ee673
valentin wimetz <38048992+valentinwmtz@users.noreply.github.com>
2023-06-04 20:33:42 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+type Match = {
+    homeTeam: string;
+    awayTeam: string;
+    date: Date;
+    odds: {
+        homeTeam: number;
+        awayTeam: number;
+    };
+};
+
+let matches: Match[] = [
+    { homeTeam: "PSG", awayTeam: "Marseille", date: new Date("2023-07-28T20:00:00"), odds: {homeTeam: 1.8, awayTeam: 2.2} },
+    { homeTeam: "Real Madrid", awayTeam: "Barcelona", date: new Date("2023-07-29T18:00:00"), odds: {homeTeam: 1.6, awayTeam: 2.4} },
+    { homeTeam: "Bayern Munich", awayTeam: "Borussia Dortmund", date: new Date("2023-08-01T15:00:00"), odds: {homeTeam: 2.0, awayTeam: 2.0} }
+];
+
+function createMatchCards(matches: Match[]) {
+    const matchCardsContainer = document.getElementById('match-cards');
+
+    matches.forEach(match => {
+
+    });
+}
+
+createMatchCards(matches);
